a43b662c2e62113604c39c3d4862b1be4ff4f693
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-07-22 19:30:48 -0400

    chore: move host specific data to moonlark folder

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
       mockingjay = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         # > Our main nixos configuration file <
-        modules = [./nixos/configuration.nix];
+        modules = [./moonlark/configuration.nix];
       };
     };
 
@@ -36,7 +36,7 @@
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
       # FIXME replace with your username@hostname
-      "kierank@mockingjay" = home-manager.lib.homeManagerConfiguration {
+      "kierank@moonlark" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
