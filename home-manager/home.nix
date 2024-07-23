10b888ee6215d257deba6f64f58c0db720771c61
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-07-22 20:01:38 -0400

    chore: update home manager configs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -17,18 +17,6 @@
   ];
 
   nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
     # Configure your nixpkgs instance
     config = {
       # Disable if you don't want unfree packages
@@ -38,10 +26,9 @@
     };
   };
 
-  # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "kierank";
+    homeDirectory = "/home/kierank";
   };
 
   # Add stuff for your user as you see fit:
