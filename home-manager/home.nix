dec6f036daa41c28a1739fcbcb820808f3fd01e8
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-07-25 09:26:06 -0400

    feat: add alacritty config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -228,6 +228,38 @@
     ];
   };
 
+  # alacritty
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      live_config_reload = true;
+      cursor = {
+        unfocused_hollow = true;
+        style = {
+          blinking = "On";
+        };
+      };
+      window = {
+        opacity = 0.75;
+        padding = {
+          x = 12;
+          y = 12;
+        };
+      };
+      font = {
+        size = 13;
+        normal = {
+          family = "JetBrainsMono Nerd Font";
+        };        
+      };
+      colors = {
+        primary = {
+          background = lib.mkForce "#1E2128";
+        };
+      };
+    };
+  };
+
   services.mako = {
     enable = true;
     defaultTimeout = 4000;
