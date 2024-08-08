b83dcd80219a276b1c1fff48860d714b4c4c7e3e
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-08-08 16:39:18 -0400

    feat: Enable Steam Remote Play, Source Dedicated Server, and Steam Local Network Game Transfers

--- moonlark/configuration.nix
+++ moonlark/configuration.nix
@@ -206,6 +206,13 @@
     ];
   };
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   programs.hyprland.enable = true;
   services.hypridle.enable = true;
   
