11ab901948e47ab74c09cec3efdfdfcfb2afb8db
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-08-08 16:22:12 -0400

    chore: Update home-manager configuration and dependencies

--- home-manager/machines/moonlark/default.nix
+++ home-manager/machines/moonlark/default.nix
@@ -42,6 +42,12 @@
   # Enable home-manager and git
   programs.home-manager.enable = true;
 
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+
 
   ###########
   # theming #
