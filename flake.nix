83e6ff9fb3dfd587c53e41c7dbf03c2a5fb0856c
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-08-14 09:53:35 -0400

    chore: update packages and remove hyprnix from this branch

--- flake.nix
+++ flake.nix
@@ -13,12 +13,6 @@
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # hyprland nix
-    Hyprnix.url = "github:hyprland-community/hyprnix";
-    hyprland-contrib = {
-      url = "github:hyprwm/contrib";
-    };
-
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     
