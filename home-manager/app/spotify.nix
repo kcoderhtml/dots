112aac91c4367da73e31bd554943cc047b750d7f
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-09-09 16:20:30 -0400

    feat: change spicetify theme to text with catpuccinMocha color scheme

--- home-manager/app/spotify.nix
+++ home-manager/app/spotify.nix
@@ -21,7 +21,7 @@
         hidePodcasts
         shuffle # shuffle+ (special characters are sanitized out of extension names)
       ];
-      theme = spicePkgs.themes.catppuccin;
-      colorScheme = "macchiato";
+      theme = spicePkgs.themes.text;
+      colorScheme = "CatppuccinMocha";
     };
 }
