4e78b844bb6a1c47015928e5634370cf070f78ec
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-12-14 18:20:16 -0500

    chore: upgrade lix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
     # Lix
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
